0c7fa20b824b8734168e63175ef02948c8d3414e
Numb3r2 <kotzur.paul@gmail.com>
2023-10-18 01:29:37 +0200

    new player_file for sql

--- files_manipulation/sql_functions.py
+++ files_manipulation/sql_functions.py
@@ -1,44 +1,25 @@
 from dotenv import dotenv_values
-import pandas as pd
 import sqlalchemy
-import mysql.connector 
 from sqlalchemy import text
+import mysql.connector 
 import psycopg2
+import pandas as pd
 
 def get_sql_config():
     '''
         Function loads credentials from .env file and
         returns a dictionary containing the data needed for sqlalchemy.create_engine()
     '''
-    needed_keys = ['host', 'port', 'database','user','password']
+    needed_keys = ['host', 'port', 'dbname','user','password']
     dotenv_dict = dotenv_values(".env")
     sql_config = {key:dotenv_dict[key] for key in needed_keys if key in dotenv_dict}
     return sql_config
 
 # Insert the get_engine() function definition below - when instructed
 def get_engine_alchemy():
-
-    engine = sqlalchemy.create_engine('mysql+mysqlconnector://user:pass@host/database',
-                    connect_args=get_sql_config())
+    engine = sqlalchemy.create_engine('postgresql://user:pass@host/dbname',
+                    connect_args=get_sql_config())    
     return engine
-
-def get_engine_mysql():
-    engine = mysql.connector.connect(**get_sql_config())
-    return engine
-
-## engine same like connection
-def get_data_mysql(query):
-    engine = get_engine_mysql()
-
-    cursor = engine.cursor()
-    cursor.execute(query)
-    results = []
-    for i, data in enumerate(cursor):
-        results.append(data)
-    cursor.close()
-    engine.close()
-    return results
-
 
 # Insert the get_data() function definition below - do this only when instructed in the notebook
 def get_data_alchemy(query):
@@ -48,8 +29,6 @@ def get_data_alchemy(query):
         results = conn.execute(query)
         return results.fetchall()
     
-
-
 
 # Insert the get_dataframe() function definition below - do this only when instructed in the notebook
 def get_dataframe(query):
@@ -73,3 +52,35 @@ def push_to_database(df, table_name, engine, schema):
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             engine = None
+
+def send(query):
+    engine = get_engine_alchemy()
+    query = text(query)                 #since sqlalchemy2.0 text is necessary
+
+    with engine.connect() as con:
+        con.execute(query)
+    #cursor = engine.cursor()
+    #cursor.execute(query)
+    #engine.commit()
+    #cursor.close()
+    #engine.close()
+    return
+
+
+
+def get_engine_mysql():
+    engine = mysql.connector.connect(**get_sql_config())
+    return engine
+
+
+def get_data_mysql(query):
+    engine = get_engine_mysql()
+
+    cursor = engine.cursor()
+    cursor.execute(query)
+    results = []
+    for i, data in enumerate(cursor):
+        results.append(data)
+    cursor.close()
+    engine.close()
+    return results
